222a6efd4cdf6384c1ef5d4dd1cd0ca209b8a42e
Patrik <patrik.tipek@email.cz>
2020-04-19 13:23:34 +0200

    first commit

---
+++ schema.js
@@ -0,0 +1,130 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLBoolean
+} = require("graphql");
+const key = require('./config');
+
+// Search
+const SearchType= new GraphQLObjectType({
+  name: "Search",
+  fields: () => ({
+    offset: { type: GraphQLInt },
+    number: { type: GraphQLInt },
+    results: { type: GraphQLList(RecipesType) },
+    totalResults: { type: GraphQLInt },
+    cuisine: { type: GraphQLString },
+  }),
+});
+
+// Recipe type
+const RecipesType = new GraphQLObjectType({
+  name: "Recipes",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    image: { type: GraphQLString },
+    readyInMinutes: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    offset: { type: GraphQLInt },
+  }),
+});
+
+// RecipeDetail type
+const RecipeDetailType = new GraphQLObjectType({
+  name: "RecipeDetail",
+  fields: () => ({
+    title: { type: GraphQLString },
+    image: { type: GraphQLString },
+    servings: { type: GraphQLInt },
+    readyInMinutes: { type: GraphQLInt },
+    vegan: { type: GraphQLBoolean },
+    vegetarian: { type: GraphQLBoolean },
+    cuisines: { type: GraphQLList(GraphQLString) },
+    dishTypes: { type: GraphQLList(GraphQLString) },
+    extendedIngredients: { type: GraphQLList(IngredientType) },
+  }),
+});
+
+// Ingredient type
+const IngredientType = new GraphQLObjectType({
+  name: "Ingredient",
+  fields: () => ({
+    aisle: { type: GraphQLString },
+    amount: { type: GraphQLFloat },
+    name: { type: GraphQLString },
+    original: { type: GraphQLString },
+    measures: { type: MeasuresType },
+  }),
+});
+
+// Measures type
+const MeasuresType = new GraphQLObjectType({
+  name: "Measures",
+  fields: () => ({
+    metric: { type: MeasureType },
+    us: { type: MeasureType },
+  }),
+});
+
+// Measure type
+const MeasureType = new GraphQLObjectType({
+  name: "Measure",
+  fields: () => ({
+    amount: { type: GraphQLFloat },
+    unitLong: { type: GraphQLString },
+    unitShort: { type: GraphQLString },
+  }),
+});
+
+// root query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    search: {
+      type: SearchType,
+      resolve(parent, args) {
+        return axios
+          .get(
+            `https://api.spoonacular.com/recipes/search?apiKey=${key.api}`
+          )
+          .then((res) => res.data);
+      },
+    },
+    recipe: {
+      type: RecipeDetailType,
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(
+            `https://api.spoonacular.com/recipes/${args.id}/information?apiKey=${key.api}`
+          )
+          .then((res) => res.data);
+      },
+    },
+    random_recipes: {
+      type: RecipeDetailType,
+      args: {
+        tags: { type: GraphQLList(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(
+            `https://api.spoonacular.com/recipes/random?number=10&tags=${args.tags}&apiKey=${key.api}`
+          )
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
